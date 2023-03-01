a6a18b510bbec0c2fc6f740f731244b7bc236c62
LuuPhamAnhKiet <kietlpagcc200083@fpt.edu.vn>
2023-03-01 23:29:55 +0700

    add action "Edit" in "AuthorsController"

--- FPTbook/Controllers/AuthorsController.cs
+++ FPTbook/Controllers/AuthorsController.cs
@@ -73,4 +73,59 @@ namespace FPTBook.Controllers
             return View(author);
         }
 
+    // GET: Authors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Author == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Author.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Author author,string Name)
+        {
+            if (id != author.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    if(_context.Author.Where(a => a.Name == Name).ToList().Count != 0){
+                     return View(author);
+                }
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+
 }

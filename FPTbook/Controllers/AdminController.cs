63dbc99a902e1c749a9046d320e071398a037f9c
tuan <tuanhqgcc200311@fpt.edu.vn>
2023-03-01 22:36:37 +0700

    Add action "Error" in "AdminController"

--- FPTbook/Controllers/AdminController.cs
+++ FPTbook/Controllers/AdminController.cs
@@ -48,6 +48,13 @@ public class AdminController : Controller
         return View();
     }
 
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
    
 
 }

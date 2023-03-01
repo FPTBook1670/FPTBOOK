6184aaca8a6be6a68c0c37bcd66f4095a09aee45
MinhGCC200321 <minhtqgcc200321@fpt.edu.vn>
2023-03-01 21:45:59 +0700

    add entity in Author

--- FPTbook/Models/Author.cs
+++ FPTbook/Models/Author.cs
@@ -2,5 +2,8 @@ namespace FPTBook.Models;
 using System.ComponentModel.DataAnnotations;
 public class Author
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<Book>? Books { get; set; }
 }

907d5fc9722ab297a61a1c5b32418f6aeb3aed27
MinhGCC200321 <minhtqgcc200321@fpt.edu.vn>
2023-03-02 21:35:15 +0700

    add action Edit by method Get and Post in Book

--- FPTbook/Controllers/BooksController.cs
+++ FPTbook/Controllers/BooksController.cs
@@ -104,5 +104,71 @@ namespace FPTBook.Controllers
             ViewData["PublisherID"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherID);
             return View(book);
         }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorID"] = new SelectList(_context.Author, "Id", "Name", book.AuthorID);
+            ViewData["CategoryID"] = new SelectList(_context.Category.Where(m => m.Status == "Approve"), "Id", "Name", book.CategoryID);
+            ViewData["PublisherID"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherID);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,AuthorID,PublisherID,Poster,CategoryID,ReleaseDate,Price")] Book book, IFormFile myfile)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(myfile.FileName);
+                    var filePath = Path.Combine(hostEnvironment.WebRootPath, "uploads");
+                    string fullPath = filePath + "\\" + filename;
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await myfile.CopyToAsync(stream);
+                    }
+                    book.Poster = filename;
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorID"] = new SelectList(_context.Author, "Id", "Name", book.AuthorID);
+            ViewData["CategoryID"] = new SelectList(_context.Category.Where(m => m.Status == "Approve"), "Id", "Name", book.CategoryID);
+            ViewData["PublisherID"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherID);
+            return View(book);
+        }
+
     }
 }
